4366a5ffa04c0493c9a950110d97b801775c1e67
iamendy <17215830+iamendy@users.noreply.github.com>
2024-10-08 12:56:22 +0100

    project init

---
+++ src/wagmi.ts
@@ -0,0 +1,26 @@
+import { http, cookieStorage, createConfig, createStorage } from "wagmi";
+import { baseSepolia } from "wagmi/chains";
+import { coinbaseWallet, injected } from "wagmi/connectors";
+
+export function getConfig() {
+  return createConfig({
+    chains: [baseSepolia],
+    connectors: [
+      injected(),
+      coinbaseWallet({ appName: "Baselink", preference: "smartWalletOnly" }),
+    ],
+    storage: createStorage({
+      storage: cookieStorage,
+    }),
+    ssr: true,
+    transports: {
+      [baseSepolia.id]: http(),
+    },
+  });
+}
+
+declare module "wagmi" {
+  interface Register {
+    config: ReturnType<typeof getConfig>;
+  }
+}
