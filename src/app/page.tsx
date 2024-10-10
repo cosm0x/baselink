1daf7052a925063188f2e51ec8825ef5579f72c4
cosm0x <120276803+cosm0x@users.noreply.github.com>
2024-10-10 15:18:36 +0100

    chore: update  homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,48 +1,75 @@
-'use client'
+"use client";
+import { useAccount, useConnect, useDisconnect } from "wagmi";
+import Container from "@/components/Container";
+import Link from "next/link";
+import { Stroke } from "@/icons";
+import { Button } from "@/components/ui/button";
 
-import { useAccount, useConnect, useDisconnect } from 'wagmi'
-
-function App() {
-  const account = useAccount()
-  const { connectors, connect, status, error } = useConnect()
-  const { disconnect } = useDisconnect()
+export default function Component() {
+  const { isConnected } = useAccount();
+  const { address } = useAccount();
+  const { connectors, connect, status, error } = useConnect();
+  const { disconnect } = useDisconnect();
 
   return (
-    <>
-      <div>
-        <h2>Account</h2>
+    <Container>
+      <div className="flex flex-col justify-center items-center">
+        <div>address: {JSON.stringify(address)}</div>
+        <section className="py-8 lg:py-8 px-4 lg:pt-10 lg:px-7 pb-16">
+          <header className="flex flex-col gap-y-12 xl:max-w-[1080px] xl:mx-auto">
+            <div className="flex flex-col ">
+              <h1
+                className="font-inter font-bold tracking-[-0.04em] leading-[1.2em] text-base-100 text-4xl text-center
+           md:mx-auto lg:text-[56px] w-full"
+              >
+                <span className="relative inline-block">
+                  Quick listings, <Stroke />
+                </span>{" "}
+                Fast payments!
+              </h1>
 
-        <div>
-          status: {account.status}
-          <br />
-          addresses: {JSON.stringify(account.addresses)}
-          <br />
-          chainId: {account.chainId}
-        </div>
+              <p
+                className="text-lg font-medium leading-[1.75em] text-center 
+          md:w-[70%] md:mx-auto lg:w-[60%] lg:text-2xl mt-12"
+              >
+                Create links and Start accepting USDT right now!
+              </p>
 
-        {account.status === 'connected' && (
-          <button type="button" onClick={() => disconnect()}>
-            Disconnect
-          </button>
-        )}
+              <div className="flex items-center justify-center mt-4">
+                {isConnected ? (
+                  <Link
+                    href="/user"
+                    className="bg-yellow font-bold text-base-100 px-5 py-3 rounded-lg lg:px-9"
+                  >
+                    Launch App →
+                  </Link>
+                ) : (
+                  <>
+                    {connectors.map((connector) => (
+                      <Button
+                        variant={"outline"}
+                        key={connector.uid}
+                        onClick={() => connect({ connector })}
+                        type="button"
+                        className="bg-yellow font-bold text-base-100 px-5 py-3 rounded-lg lg:px-9"
+                      >
+                        {connector.name == "Injected"
+                          ? "Connect wallet → "
+                          : "I dont have a wallet"}
+                      </Button>
+                    ))}
+                  </>
+                )}
+              </div>
+              <div className="text-center">{error?.message}</div>
+            </div>
+
+            <div className="flex items-center justify-center">
+              <div className="w-[60%]">Welcome to BaseLink</div>
+            </div>
+          </header>
+        </section>
       </div>
-
-      <div>
-        <h2>Connect</h2>
-        {connectors.map((connector) => (
-          <button
-            key={connector.uid}
-            onClick={() => connect({ connector })}
-            type="button"
-          >
-            {connector.name}
-          </button>
-        ))}
-        <div>{status}</div>
-        <div>{error?.message}</div>
-      </div>
-    </>
-  )
+    </Container>
+  );
 }
-
-export default App
