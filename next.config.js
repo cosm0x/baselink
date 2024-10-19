729506ce348d8688510004fcded5d7cb5aa29c49
iamendy <17215830+iamendy@users.noreply.github.com>
2024-10-19 17:06:24 +0100

    fix: prod bug

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = { transpilePackages: ["supports-color"] };
 
-module.exports = nextConfig
+module.exports = nextConfig;
